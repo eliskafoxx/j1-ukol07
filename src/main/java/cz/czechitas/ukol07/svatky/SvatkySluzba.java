0cb2ff2961a1579f4e322e3d9f6a16b13b445d6d
eliskafoxx <eliska.foxx@gmail.com>
2023-04-22 18:21:29 +0200

    vsechny TODO implementovany

--- src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.MonthDay;
 import java.util.List;
+
 
 public class SvatkySluzba {
 
@@ -18,10 +20,9 @@ public class SvatkySluzba {
     private final SeznamSvatku seznamSvatku;
 
     public SvatkySluzba() throws IOException {
-        // TODO načíst seznam svátků ze souboru svatky.json
 
-        // Následující řádek po vlastní implementaci smažete.
-        seznamSvatku = null;
+        String jsonContent = Files.readString(cestaKDatum);
+        seznamSvatku = objectMapper.readValue(jsonContent, SeznamSvatku.class);
     }
 
     public List<String> vyhledatSvatkyDnes() {
@@ -29,14 +30,10 @@ public class SvatkySluzba {
     }
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
-        // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
-
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
+        return seznamSvatku.getSvatky().stream()
+                .filter(s -> s.getDen().equals(day))
+                .map(Svatek::getJmeno)
+                .toList();
     }
 }
+
